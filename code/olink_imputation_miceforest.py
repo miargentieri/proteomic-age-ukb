fa85298d7bd9e7d99219fed3b99d398f49e46772
Austin Argentieri <64753618+miargentieri@users.noreply.github.com>
2024-08-07 20:43:48 +0200

    Add files via upload

---
+++ code/olink_imputation_miceforest.py
@@ -0,0 +1,36 @@
+import miceforest as mf
+import pandas as pd
+import datetime as dt
+
+random_seed = 3456
+
+# Load protein data
+data_path = '/gpfs3/well/charge/shared/ukb_phenotype_61054/olink_data/olink_data_wide_oct_30_2023.csv'
+data = pd.read_csv(data_path)
+data = data[data.columns[data.columns != 'X']]
+
+# Create a dictionary of variables use to impute
+exclude = ['eid', 'olink_batch', 'olink_plate', 'GLIPR1', 'NPM1', 'PCOLCE']
+dont_impute = ['eid', 'olink_batch', 'olink_plate']
+column_dict = {col: [other_col for other_col in data.columns if other_col != col and other_col not in exclude] for col in data.columns if col not in dont_impute}
+
+# run miceforest imputation on multiple cores
+kds = mf.ImputationKernel(
+  data,
+  datasets=1,
+  variable_schema=column_dict,
+  random_state=random_seed
+)
+
+# run
+kds.mice(
+  iterations=5,
+  n_jobs=-1, 
+  verbose=True
+)
+
+# get the completed dataframe from the miceforest object
+olink_data_imputed = kds.complete_data()
+
+name = "/gpfs3/well/charge/shared/ukb_phenotype_61054/olink_data/olink_data_imputed_nov_18_2023.csv"
+olink_data_imputed.to_csv(name, index=False)
